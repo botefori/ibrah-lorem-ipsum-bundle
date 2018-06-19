5cf640eacf411f6f42da74051f64f4a255ae03a2
Ibrahima <icamara@wynd.eu>
2018-06-19 10:14:36 +0200

    setup functional tests

---
+++ tests/Service/FunctionalTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: icamara
+ * Date: 19/06/18
+ * Time: 10:12
+ */
+
+namespace KnpU\LoremIpsumIbrahBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FunctionalTest extends TestCase
+{
+
+}
