ef388025eaf530b7a378272ac0900e7a1c2d1a87
Ibrahima <icamara@wynd.eu>
2018-06-19 15:07:00 +0200

    debug service configuration bug from service xml file

--- tests/Service/FunctionalTest.php
+++ tests/Service/FunctionalTest.php
@@ -3,9 +3,41 @@ declare(strict_types=1);
 
 namespace KnpU\LoremIpsumIbrahBundle\Tests;
 
+use KnpU\LoremIpsumIbrahBundle\KnpUIpsum;
+use KnpU\LoremIpsumIbrahBundle\KnpULoremIpsumIbrahBundle;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class FunctionalTest extends TestCase
 {
+
+    public function testServiceWiring()
+    {
+       $kernel = new KnpULoremIpsumTestingKernel('test', true);
+       $kernel->boot();
+       $container = $kernel->getContainer();
+
+       $ipsum = $container->get('knpu_lorem_ipsum.knpu_ipsum');
+       $this->assertInstanceOf(KnpUIpsum::class, $ipsum);
+       $this->assertInternalType('string', $ipsum->getParagraphs());
+    }
+}
+
+class KnpULoremIpsumTestingKernel extends Kernel{
+
+
+    public function registerBundles()
+    {
+        return [
+            new KnpULoremIpsumIbrahBundle()
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        // TODO: Implement registerContainerConfiguration() method.
+    }
 
 }
